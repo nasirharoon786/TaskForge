e240cb17bd4bdec083f7dc4e77bc3afdd345b7a1
nasirharoon786 <nasirharoon786@gmail.com>
2025-03-04 13:20:53 +0500

    Pinia -  definedStore function with arrow function

--- frontend/src/stores/task.js
+++ frontend/src/stores/task.js
@@ -1,32 +1,43 @@
+import { ref, reactive, computed } from "vue";
 import { defineStore } from "pinia";
 import { allTasks } from "@/http/task-api";
 
-export const useTaskStore = defineStore("taskStore", {
-  state: () => ({
-    tasks: [],
-    task: {
-      id: 1,
-      name: "Task 1",
-      is_completed: false,
-    },
-  }),
-  getters: {
-    completedTasks: (state) => state.tasks.filter((task) => task.is_completed),
-    uncompletedTasks() {
-      return this.tasks.filter((task) => !task.is_completed);
-    },
-  },
-  actions: {
-    async fetchAllTasks() {
-        const { data } = await allTasks();
-        this.tasks = data.data;
-    },
-    addTask(task) {
-      this.tasks.push(task);
-    },
-    removeTask(task) {
-      const index = this.tasks.indexOf(task);
-      this.tasks.splice(index, 1);
-    },
-  },
+// const tmp = {
+//   state: () => ({}),
+//   getters: {},
+//   actions: {
+//     async fetchAllTasks() {
+//       const { data } = await allTasks();
+//       this.tasks = data.data;
+//     },
+//     addTask(task) {
+//       this.tasks.push(task);
+//     },
+//     removeTask(task) {
+//       const index = this.tasks.indexOf(task);
+//       this.tasks.splice(index, 1);
+//     },
+//   },
+// };
+export const useTaskStore = defineStore("taskStore", () => {
+  const tasks = ref([]);
+
+  const completedTasks = computed(() =>
+    tasks.value.filter((task) => task.is_completed)
+  );
+  const uncompletedTasks = computed(() =>
+    tasks.value.filter((task) => !task.is_completed)
+  );
+
+  const fetchAllTasks = async () => {
+    const { data } = await allTasks();
+    tasks.value = data.data;
+  };
+
+  return {
+    tasks,
+    completedTasks,
+    uncompletedTasks,
+    fetchAllTasks,
+  };
 });
